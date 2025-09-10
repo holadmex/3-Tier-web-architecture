1cbf9dd0275e6d6681369e8d927777686b1e1c3e
holadmex <ojewumidimeji@gmail.com>
2025-09-11 00:26:30 +0100

    eks deployment

--- ecs-terraform/iam.tf
+++ ecs-terraform/iam.tf
@@ -18,43 +18,16 @@ resource "aws_iam_role" "ecs_task_execution_role" {
 # Attach the AmazonECSTaskExecutionRolePolicy (default)
 resource "aws_iam_policy_attachment" "ecs_task_execution_attachment" {
   name       = "ecs-task-execution-policy"
-  roles      = [aws_iam_role.ecs_task_execution_role.name]
+  roles      = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-# Add combined inline policy for CloudWatch Logs and ECR permissions
-resource "aws_iam_role_policy" "ecs_task_execution_custom_policy" {
-  name = "ecs-task-custom-policy"
-  role = aws_iam_role.ecs_task_execution_role.name
+resource "aws_iam_role_policy_attachment" "amazon_ecs_full_access" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonECS_FullAccess"
+}
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      # CloudWatch Logs permissions
-      {
-        Action = [
-          "logs:CreateLogStream",
-          "logs:PutLogEvents",
-          "logs:CreateLogGroup",
-          "logs:DescribeLogStreams"
-        ]
-        Effect   = "Allow"
-        Resource = ["arn:aws:logs:us-east-1:429841094792:log-group:/ecs/frontend/*", "*"]
-      },
-      # ECR permissions
-      {
-        Effect   = "Allow"
-        Action   = "ecr:GetAuthorizationToken"
-        Resource = "*"
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "ecr:BatchGetImage",
-          "ecr:GetDownloadUrlForLayer"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
+resource "aws_iam_role_policy_attachment" "amazon_ecr_full_access" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonElasticContainerRegistryPublicFullAccess"
 }
